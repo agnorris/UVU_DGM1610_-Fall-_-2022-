0b7f9a2b4608b88072fbe4eb4f18d404ae848ce6
agnorris <112352513+agnorris@users.noreply.github.com>
2022-09-20 21:10:01 -0600

    While loop practice

---
+++ Lab 3.cs
@@ -0,0 +1,30 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int i = 0;
+		while (i < 5)
+		{
+			Console.WriteLine("i");
+			i++;
+		}
+	}
+}
+
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int i = 0;
+		do
+		{
+			Console.WriteLine("i");
+			i++;
+		}
+		while (i < 5);
+	}
+}
