8d85680db4cdb713240e5a22cd836e9d09e94b29
agnorris <112352513+agnorris@users.noreply.github.com>
2022-09-20 21:18:41 -0600

    For Loop

--- Lab 3.cs
+++ Lab 3.cs
@@ -28,3 +28,43 @@ public class Program
 		while (i < 5);
 	}
 }
+
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		for (int i = 0; i <5; i++)
+		{
+			Console.WriteLine("i");
+		}
+	}
+}
+
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		for (int i = 0; i <10; i=i+2)
+		{
+			Console.WriteLine("i");
+		}
+	}
+}
+
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		string[] dogs = {"Poodle", "Golden Retriever", "German Shepherd", "Dalmation"};
+		foreach (string dog in dogs)
+		{
+			Console.WriteLine(dog);
+		}
+	}
+}
