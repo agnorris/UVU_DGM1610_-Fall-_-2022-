db375df493d699e42821f92a11fdb116710909ca
Aubrey Gallafent <agnorris@gmail.com>
2022-10-28 18:14:58 -0600

    Player Movement

--- Unity-Projects/Unity Project/Assets/Unit-6-Challenge/Scripts/PowerupBehavior.cs
+++ Unity-Projects/Unity Project/Assets/Unit-6-Challenge/Scripts/PowerupBehavior.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,25 +7,11 @@ public class PowerupBehavior : MonoBehaviour
 {
     public float speed = 10.0f;
     private Rigidbody _powerup;
-    private GameObject powerup;
 
-    void Start()
+    private void Start()
     {
-        Instantiate(powerup);
-        GetComponent<Renderer>().enabled = false;
-        powerup.SetActive(false);
-    }
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            powerup.GetComponent<Renderer>().enabled = true;
-            powerup.SetActive(true);
-        }
         _powerup = this.GetComponent<Rigidbody>();
         _powerup.velocity = new Vector3(-speed, 0, 0);
     }
 
     }
-}
