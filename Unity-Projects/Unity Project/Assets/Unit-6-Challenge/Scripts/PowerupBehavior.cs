8c7e75ed571abfc74bce6ef741d3debfa753b5da
Aubrey Gallafent <agnorris@gmail.com>
2022-11-04 15:57:51 -0600

    Powerup movement troubleshoot

--- Unity-Projects/Unity Project/Assets/Unit-6-Challenge/Scripts/PowerupBehavior.cs
+++ Unity-Projects/Unity Project/Assets/Unit-6-Challenge/Scripts/PowerupBehavior.cs
@@ -1,17 +1,15 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UIElements;
 using UnityEngine;
 public class PowerupBehavior : MonoBehaviour
 
 {
-    public float speed = 10.0f;
-    private Rigidbody _powerup;
-
-    private void Start()
+    public float speed = 10f;
+    private void Update()
     {
-        _powerup = this.GetComponent<Rigidbody>();
-        _powerup.velocity = new Vector3(-speed, 0, 0);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed); 
     }
 
     }
