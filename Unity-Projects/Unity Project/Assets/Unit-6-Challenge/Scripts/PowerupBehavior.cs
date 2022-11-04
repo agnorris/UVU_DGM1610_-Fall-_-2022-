585e47ff211d577065571c076a242d4dfa28b248
Aubrey Gallafent <agnorris@gmail.com>
2022-11-04 17:08:10 -0600

    Flash effect

--- Unity-Projects/Unity Project/Assets/Unit-6-Challenge/Scripts/PowerupBehavior.cs
+++ Unity-Projects/Unity Project/Assets/Unit-6-Challenge/Scripts/PowerupBehavior.cs
@@ -1,15 +1,27 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor.UIElements;
 using UnityEngine;
 public class PowerupBehavior : MonoBehaviour
 
 {
     public float speed = 10f;
-    private void Update()
+    public GameObject health;
+    public Color startcolor = Color.white;
+    public Color endcolor = Color.red;
+    public float interval = 1f;
+
+    private void Awake()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed); 
+        GetComponent<Renderer>();
     }
 
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        GetComponent<Renderer>().material.color = Color.Lerp(startcolor, endcolor, Mathf.PingPong(Time.time, 1));
     }
+    
+}
