bba4f430539e22d09d07bcd60113b4d8cb7e1c3c
Aubrey Gallafent <agnorris@gmail.com>
2022-11-04 15:32:37 -0600

    Troubleshoot x movement

--- Unity-Projects/Unity Project/Assets/Unit-6-Challenge/Scripts/PlayerController.cs
+++ Unity-Projects/Unity Project/Assets/Unit-6-Challenge/Scripts/PlayerController.cs
@@ -10,13 +10,34 @@ namespace Unit_6_Challenge.Scripts
         public float horizontalInput;
         public float speed;
         public float verticalInput;
+        public float xRange;
+        public float zRange;
         public GameObject powerup;
         void Update()
         {
             horizontalInput = Input.GetAxis("Horizontal");
             verticalInput = Input.GetAxis("Vertical");
             transform.Translate(Vector3.right * (horizontalInput * speed));
-            transform.Translate(Vector3.up * (verticalInput * speed));
+            transform.Translate(Vector3.forward * (verticalInput * speed));
+            
+            if (transform.position.x < -xRange)
+            {
+                transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+            }
+
+            if (transform.position.x > xRange)
+            {
+                transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+            }
+            if (transform.position.x < -zRange)
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y, -zRange);
+            }
+
+            if (transform.position.x > zRange)
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+            }
 
         }
 
