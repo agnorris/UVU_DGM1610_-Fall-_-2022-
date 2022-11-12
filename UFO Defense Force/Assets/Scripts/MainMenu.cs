7589bf57722975f4840aea729a692200398d2fa5
Aubrey Gallafent <agnorris@gmail.com>
2022-11-11 21:52:48 -0700

    Button sound

--- UFO Defense Force/Assets/Scripts/MainMenu.cs
+++ UFO Defense Force/Assets/Scripts/MainMenu.cs
@@ -6,16 +6,19 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public int sceneToLoad;
+    public AudioSource buttonSound;
 
     public void StartGame()
     {
         SceneManager.LoadScene(sceneToLoad);
+        buttonSound.Play(0);
         Debug.Log("New Scene Loaded!");
     }
     
     public void QuitGame()
     {
         Application.Quit();
+        buttonSound.Play(0);
         Debug.Log("Quit Game!");
     }
 }
