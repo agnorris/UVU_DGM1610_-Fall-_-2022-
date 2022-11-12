05b260c0d35d8ef95e0ac644d4fd5ac6a1afd6fe
Aubrey Gallafent <agnorris@gmail.com>
2022-11-11 21:43:03 -0700

    Missing changes

---
+++ UFO Defense Force/Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public bool isGameOver;
+    private GameObject gameOverText;
+    public AudioSource backgroundMusic;
+    public AudioSource endSound;
+    void Awake()
+    {
+        Time.timeScale = 1;
+        isGameOver = false;
+        backgroundMusic.Play(0); 
+    }
+
+    private void Start()
+    {
+        gameOverText = GameObject.Find("GameOverText");
+    }
+
+    void Update()
+    {
+        if (isGameOver)
+        {
+            EndGame();
+        }
+        else
+        {
+            gameOverText.gameObject.SetActive(false);
+        }
+    }
+
+    public void EndGame()
+    {
+        gameOverText.gameObject.SetActive(true);
+        endSound.Play(0);
+        Time.timeScale = 0;
+    }
+}
