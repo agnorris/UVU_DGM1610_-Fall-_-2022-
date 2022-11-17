be90d9c2c0b59b19f8060bc8922b61580e453bfc
Aubrey Gallafent <agnorris@gmail.com>
2022-11-16 22:49:42 -0700

    Second half lab

---
+++ Game 2/Assets/Scripts/TextLabelBehavior.cs
@@ -0,0 +1,31 @@
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace Game2
+{
+    [RequireComponent(typeof(Text))]
+
+    public class TextLabelBehavior : MonoBehaviour
+    {
+        public Text label;
+        public UnityEvent startEvent;
+
+        private void Start()
+        {
+            label = GetComponent<Text>();
+            startEvent.Invoke();
+        }
+
+        public void UpdateLabel(FloatData obj)
+        {
+            label.text = obj.value.ToString(CultureInfo.InvariantCulture);
+        }
+
+        public void UpdateLabel(IntData obj)
+        {
+            label.text = obj.value.ToString(CultureInfo.InvariantCulture);
+        }
+    }
+}
