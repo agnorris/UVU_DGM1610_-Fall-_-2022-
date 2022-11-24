e7cc76e824a60d833ccbd1359b5dfba9cf945e38
Aubrey Gallafent <agnorris@gmail.com>
2022-11-23 21:16:46 -0700

    Basic ID object created with debug MatchBehavior

---
+++ Game 2 #2/Assets/Scripts/MatchBehavior.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchBehavior : MonoBehaviour
+{
+    public ID idObj;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(idObj);
+    }
+}
