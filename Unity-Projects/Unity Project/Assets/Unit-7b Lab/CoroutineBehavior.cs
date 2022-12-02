bb53ae33a5f89e239a7b76a305b9c95026ca10c3
Aubrey Gallafent <agnorris@gmail.com>
2022-12-01 20:21:06 -0700

    Commit a whole bunch of uncommitted files

---
+++ Unity-Projects/Unity Project/Assets/Unit-7b Lab/CoroutineBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CoroutineBehavior : MonoBehaviour
+{
+    public UnityEvent repeatEvent;
+
+    public int counterNum = 3;
+    public float seconds = 3.0f;
+    private WaitForSeconds wfsobj;
+    private WaitForFixedUpdate wffuobj;
+
+    IEnumerator Start()
+    {
+        wfsobj = new WaitForSeconds(seconds);
+        wffuobj = new WaitForFixedUpdate();
+
+        while (counterNum > 0)
+        {
+            yield return wfsobj;
+            repeatEvent.Invoke();
+        }
+    }
+}
