e8131810de0aa22fad6b27adb774f34011e9e3da
Aubrey Gallafent <agnorris@gmail.com>
2022-12-01 17:19:39 -0700

    Working through challenge #1

--- Game 2 #2/Assets/Scripts/MatchBehavior.cs
+++ Game 2 #2/Assets/Scripts/MatchBehavior.cs
@@ -2,13 +2,28 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class MatchBehavior : MonoBehaviour
 {
     public ID idObj;
+    public UnityEvent matchEvent, noMatchEvent, noMatchDelayedEvent;
 
-    private void OnTriggerEnter(Collider other)
+    private IEnumerator OnTriggerEnter(Collider other)
     {
-        Debug.Log(idObj);
+        var tempObj = other.GetComponent<IDContainerBehavior>();
+        if (tempObj == null)
+            yield break;
+        var otherID = tempObj.idObj;
+        if (otherID == idObj)
+        {
+            matchEvent.Invoke();
+        }
+        else
+        {
+            noMatchEvent.Invoke();
+            yield return new WaitForSeconds(0.5f);
+            noMatchDelayedEvent.Invoke();
+        }
     }
 }
